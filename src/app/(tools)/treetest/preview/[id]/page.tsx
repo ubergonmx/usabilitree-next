20f4f3f691b0ef5726a8f39099b4adc69ce47f0d
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-11-30 21:59:28 +0800

    Remove user requirement in tree test preview

--- src/app/(tools)/treetest/preview/[id]/page.tsx
+++ src/app/(tools)/treetest/preview/[id]/page.tsx
@@ -1,15 +1,6 @@
-import { redirect } from "next/navigation";
-import { Paths } from "@/lib/constants";
 import TestPreviewPage from "../_components/preview-tree-test";
-import { getCurrentUser } from "@/lib/auth/session";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const user = await getCurrentUser();
-
-  if (!user) {
-    return redirect(Paths.Login);
-  }
-
   return (
     <div>
       <TestPreviewPage params={params} />
