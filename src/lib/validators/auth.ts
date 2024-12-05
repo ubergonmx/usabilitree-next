124052217d78b681bc10ae25d913b2e9a0902674
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-12-05 15:01:36 +0800

    Fix minimum password when signing up

--- src/lib/validators/auth.ts
+++ src/lib/validators/auth.ts
@@ -2,13 +2,13 @@ import { z } from "zod";
 
 export const signupSchema = z.object({
   email: z.string().email("Please enter a valid email"),
-  password: z.string().min(1, "Please provide your password.").max(255),
+  password: z.string().min(8, "Password is too short. Minimum 8 characters required.").max(255),
 });
 export type SignupInput = z.infer<typeof signupSchema>;
 
 export const loginSchema = z.object({
   email: z.string().email("Please enter a valid email."),
-  password: z.string().min(8, "Password is too short. Minimum 8 characters required.").max(255),
+  password: z.string().min(8, "Password is too short.").max(255),
 });
 export type LoginInput = z.infer<typeof loginSchema>;
 
